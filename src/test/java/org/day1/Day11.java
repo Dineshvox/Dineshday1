8c76c6377249fc6680ad83cf04269bf97895f0dd
Dineshvox <dineshmurugan23.07@gmail.com>
2022-02-01 14:54:27 +0530

    start 1

---
+++ src/test/java/org/day1/Day11.java
@@ -0,0 +1,11 @@
+package org.day1;
+
+public class Day11 {
+	public static void main(String[] args) {
+		
+		System.out.println("The first");
+		System.out.println("The second");
+		System.out.println("The three");
+	}
+
+}
